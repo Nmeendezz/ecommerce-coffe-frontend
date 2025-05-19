a066a2b28668ee1a5dec08164e2eeafc3b466eb1
Rida <raharrar12@gmail.com>
2025-05-19 12:53:04 +0200

    Agregado Footer_001

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const urbanist = Urbanist({
   subsets: ["latin"]
@@ -21,8 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={urbanist.className}>
-          <Navbar />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
